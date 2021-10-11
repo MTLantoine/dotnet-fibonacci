1ad430f2b659843e2a477619866e33549f758afd
MTLantoine <mortelier.antoine@hotmail.fr>
2021-10-11 15:43:25 +0200

    update database

---
+++ src/Fibonacci/Compute.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fibonacci
+{
+    public class Compute
+    {
+
+        public static async Task<List<long>> ExecuteAsync(string[] arguments)
+        {
+            using var dataContext = new FibonacciDataContext();
+            var results = await RunFibonaccisAsync(dataContext, arguments);
+            await dataContext.SaveChangesAsync();
+            return results;
+        }
+        
+        private static long Fib(int i) =>
+            i switch
+            {
+                int when i <= 2 => 1,
+                _ =>  Fib(i - 2) + Fib(i - 1)
+            };
+        
+        private static async Task<List<long>> RunFibonaccisAsync(FibonacciDataContext dataContext ,string[] strings)
+        {
+            var list = new List<long>();
+            foreach (var input in strings)
+            {
+                var inputInt = Convert.ToInt32(input);
+                var fibonacciFromDatabase =
+                    await dataContext
+                        .TFibonaccis
+                        .Where(tf => tf.FibInput == inputInt)
+                        .FirstOrDefaultAsync();
+
+                if (fibonacciFromDatabase != null)
+                {
+                    list.Add(fibonacciFromDatabase.FibOutput);
+                }
+                else
+                {
+                    var fibOutput = await Task.Run(() => Fib(inputInt));
+
+                    dataContext.TFibonaccis.Add(new TFibonacci()
+                    {
+                        FibInput = inputInt,
+                        FibOutput = fibOutput,
+                        FibCreatedTimestamp = DateTime.Now
+                    });
+                    list.Add(fibOutput);
+                }
+                
+            }
+
+            return list;
+        }
+    }
+}
---
+++ src/Fibonacci/Migrations/20211011132906_AddFibFibonacciCreatedTimestamp.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fibonacci.Migrations
+{
+    public partial class AddFibFibonacciCreatedTimestamp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FibCreatedTimestamp",
+                schema: "sch_fib",
+                table: "T_Fibonacci",
+                newName: "FIB_CreatedTimestamp");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FIB_CreatedTimestamp",
+                schema: "sch_fib",
+                table: "T_Fibonacci",
+                newName: "FibCreatedTimestamp");
+        }
+    }
+}
